286a2f0086a48c4f35ba9d1d6ca0fc52c111c50c
Bugi-Sulistiyo <71893631+Bugi-Sulistiyo@users.noreply.github.com>
2023-12-09 08:53:06 +0700

    completed the merging rimone

--- main/scripts/action/data_prep.py
+++ main/scripts/action/data_prep.py
@@ -33,5 +33,39 @@ def create_destination_directory(path:str,
                                                                         label_name))
     return result_status
 
-def get_file_name(path):
-    return os.path.basename(path)
+def get_file_names(path:str):
+    """generate a list of file name from the given directory
+
+    Args:
+        path (str): source directory
+
+    Returns:
+        list: list of file name in the given directory
+    """
+    return [file_name for file_name in os.listdir(path) if os.path.isfile(os.path.join(path, file_name))]
+
+def copy_files(source_path:str,
+               destination_path:str,
+               file_names:list):
+    """copy files from source directory to destination directory
+
+    Args:
+        source_path (str): source directory
+        destination_path (str): destination directory
+        file_names (list): list of file names to be copied
+
+    Returns:
+        result_status (dict): result status of the action
+    """
+    result_status = {
+        "Success": [],
+        "Already Exists": [],
+    }
+    for file_name in file_names:
+        try:
+            shutil.copyfile(os.path.join(source_path, file_name),
+                            os.path.join(destination_path, file_name))
+            result_status["Success"].append(os.path.join(destination_path, file_name))
+        except FileExistsError:
+            result_status["Already Exists"].append(os.path.join(destination_path, file_name))
+    return result_status
