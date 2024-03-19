dd6c4cefa62c6105b9b856625a9cd5a5e5acbe87
Bugi-Sulistiyo <71893631+Bugi-Sulistiyo@users.noreply.github.com>
2024-03-19 15:03:13 +0800

    refine the dashboard action module

--- main/scripts/action/dashboard.py
+++
@@ -1,3 +0,0 @@
-import os
-
-import streamlit as st
---
+++ main/scripts/action/evaluation.py
@@ -0,0 +1,10 @@
+import os
+
+import pandas as pd
+import numpy as np
+
+from PIL import Image
+import matplotlib.pyplot as plt
+
+import streamlit as st
+
