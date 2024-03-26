2085f10f81b654757a30a3c4b5b6275a3934d154
Bugi-Sulistiyo <71893631+Bugi-Sulistiyo@users.noreply.github.com>
2024-03-26 12:29:04 +0800

    add the performance model result

--- main/scripts/d. evaluation/evaluation_dashboard.py
+++ main/scripts/d. evaluation/evaluation_dashboard.py
@@ -14,10 +14,14 @@ import evaluation
 ### main path
 PATH_DATA = os.environ.get('PATH_DATA_DASHBOARD')
 PATH_DATASET = os.environ.get('PATH_DATASET_COMBINED')
+PATH_SAMPLE = os.environ.get('PATH_DATA_AUG_SAMPLE')
+PATH_EV_RESULT = os.environ.get('PATH_EVALUATION_RESULT')
 ### data path
 PATH_AUTHOR_DATA = os.path.join(PATH_DATA, 'author_info.csv')
 PATH_DATASET_DATA = os.path.join(PATH_DATA, 'dataset_info.csv')
 PATH_ARSITEKTUR_DATA = os.path.join(PATH_DATA, 'arsitektur_info.csv')
+PATH_SKENARIO_DATA = os.path.join(PATH_DATA, 'rfn_scenario_guide.csv')
+PATH_AUG_IMG = os.path.join(PATH_DATA, 'example_aug_imgs.csv')
 ### dataset path
 PATH_RIMONE = os.path.join(PATH_DATASET, 'rimone')
 PATH_G1020 = os.path.join(PATH_DATASET, 'g1020')
@@ -28,6 +32,9 @@ LOREM_IPSUM100 = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec
 LOREM_IPSUM10 = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec vitae lobortis arcu"
 THESIS_TITLE = "Perbandingan Arsitektur MobileNet dan EfficientNet pada Klasifikasi Glaukoma Berdasarkan Citra Fundus"
 LABELS_USED = ["normal", "glaukoma"]
+AUG_LIST = {'bright':'bright',
+            'horizontal flip':'h_flip',
+            'vertikal flip':'v_flip'}
 
 # function section
 def show_img(path:str,
@@ -57,6 +64,66 @@ def example_img(path:str,
     with col_glaucoma_image:
         show_img(path=path, label=label[1])
 
+def scenario_img(scenario:int,
+                augment_type:dict):
+    """showing the image for specific scenario and augmentation type
+
+    Args:
+        scenario (int): the scenario of the image
+        augment_type (dict): a dictionary of the augmentation type
+    """
+    for aug_name, aug_type in augment_type.items():
+        st.image(evaluation.img_scen_list(df_path=PATH_AUG_IMG,
+                                        img_path=PATH_SAMPLE,
+                                        scenario=scenario,
+                                        augment_type=aug_type),
+                caption=aug_name.title())
+
+def show_df_result(file_name:str):
+    """showing the dataframe result in the dashboard
+
+    Args:
+        file_name (str): the name of the file saved in the result folder
+    """
+    # import the dataframe
+    df_eval = evaluation.load_df(path=os.path.join(PATH_EV_RESULT, f'{file_name}.csv'))
+    
+    # drop the unnecessary columns with ignore error
+    df_eval.drop(columns=['true_positive', 'true_negative',
+                            'false_positive', 'false_negative'],
+                inplace=True, errors='ignore')
+    # change the column name to uppercase
+    df_columnns = [evaluation.string_upper(name) for name in df_eval.columns]
+    df_eval.columns = df_columnns
+
+    # show the dataframe
+    st.dataframe(df_eval.style \
+                    .highlight_max(axis=0, subset=df_eval.columns[-4:],
+                                    props='color:black;background-color:#EE4540') \
+                    .format(evaluation.string_upper),
+                hide_index=True, use_container_width=True, height=175)
+
+def show_val_result(name:str):
+    """a template to show the evaluation result in the dashboard
+
+    Args:
+        name (str): the name of the scenario or dataset
+    """
+    # table result
+    st.markdown(f'<div style="text-align: center; color: black"><b>Hasil Evaluasi Secara Umum</b></div>',
+                unsafe_allow_html=True)
+    show_df_result(f'{name}_general_result')
+    st.markdown(f'<div style="text-align: center; color: black"><b>Hasil Evaluasi Akhir</b></div>',
+                unsafe_allow_html=True)
+    show_df_result(f'{name}_summarize_result')
+    
+    # confusion matrix
+    st.markdown(f'<div style="text-align: center; color: black"><b>Confusion Matrix Model Terbaik</b></div>',
+                unsafe_allow_html=True)
+    st.image(os.path.join(PATH_EV_RESULT, f'confusion_matrix_{name}_general_best.png'),
+            use_column_width=True)
+
+
 # dasboard section
 ## summary and title
 with st.container():
@@ -65,60 +132,111 @@ with st.container():
     st.markdown(f"<div style='text-align: justify;'>{LOREM_IPSUM100}</div>",
                 unsafe_allow_html=True)
 
-## info dataset
-with st.container():
-    ### dataset summary
-    dataset_info_df = evaluation.load_df(path=PATH_DATASET_DATA)
-    st.header("Dataset", anchor="info-dataset")
-    st.markdown(f"<div style='text-align: justify;'>{LOREM_IPSUM10}</div><br>",
-                unsafe_allow_html=True)
-    st.dataframe(dataset_info_df.loc[:, ~dataset_info_df.columns.isin(['Link', 'Sumber'])])
+# ## info dataset
+# with st.container():
+#     ### dataset summary
+#     dataset_info_df = evaluation.load_df(path=PATH_DATASET_DATA)
+#     st.header("Dataset", anchor="info-dataset")
+#     st.markdown(f"<div style='text-align: justify;'>{LOREM_IPSUM10}</div><br>",
+#                 unsafe_allow_html=True)
+#     st.dataframe(dataset_info_df.loc[:, ~dataset_info_df.columns.isin(['Link', 'Sumber'])],
+#                 hide_index=True, use_container_width=True)
 
-    ### example image
-    st.subheader('Contoh Data Citra', anchor=f'image-dataset')
-    tab_rimone, tab_g1020, tab_refuge, tab_papila = st.tabs(["RIM-ONE",
-                                                            "G1020",
-                                                            "REFUGE",
-                                                            "PAPILA"])
+#     ### example image
+#     st.subheader('Contoh Data Citra', anchor=f'image-dataset')
+#     tab_rimone, tab_g1020, tab_refuge, tab_papila = st.tabs(["RIM-ONE",
+#                                                             "G1020",
+#                                                             "REFUGE",
+#                                                             "PAPILA"])
 
-    #### rimone dataset
-    with tab_rimone:
-        example_img(path=PATH_RIMONE,
-                    label=LABELS_USED)
-    #### g1020 dataset
-    with tab_g1020:
-        example_img(path=PATH_G1020,
-                    label=LABELS_USED)
-    #### refuge dataset
-    with tab_refuge:
-        example_img(path=PATH_REFUGE,
-                    label=LABELS_USED)
-    #### papila dataset
-    with tab_papila:
-        example_img(path=PATH_PAPILA,
-                    label=LABELS_USED)
+#     with tab_rimone:
+#         example_img(path=PATH_RIMONE,
+#                     label=LABELS_USED)
+#     with tab_g1020:
+#         example_img(path=PATH_G1020,
+#                     label=LABELS_USED)
+#     with tab_refuge:
+#         example_img(path=PATH_REFUGE,
+#                     label=LABELS_USED)
+#     with tab_papila:
+#         example_img(path=PATH_PAPILA,
+#                     label=LABELS_USED)
 
-## scenario info
-with st.container():
-    st.header("Skenario", anchor="info-skenario")
-    st.markdown(f"<div style='text-align: justify;'>{LOREM_IPSUM100}</div><br>",
-                unsafe_allow_html=True)
+# ## scenario info
+# with st.container():
+#     st.header("Skenario", anchor="info-scenario")
+#     st.markdown(f"<div style='text-align: justify;'>{LOREM_IPSUM100}</div><br>",
+#                 unsafe_allow_html=True)
+#     scenario_guide_df = evaluation.load_df(path=PATH_SKENARIO_DATA)
+#     st.dataframe(scenario_guide_df.style.hide(),
+#                 hide_index=True, use_container_width=True)
     
-    ## each scenario
-    tab_s1, tab_s2, tab_s3 = st.tabs(["Skenario 1",
-                                        "Skenario 2",
-                                        "Skenario 3"])
+#     ## each scenario
+#     st.subheader('Citra Hasil Augmentasi', anchor='image-scenario')
+#     tab_s1, tab_s2, tab_s3 = st.tabs(["Skenario 1",
+#                                         "Skenario 2",
+#                                         "Skenario 3"])
+    st.markdown("""
+        <style>
+            .stTabs [data-baseweb="tab-panel"]{
+                background-color: white;
+            }
+            .st-emotion-cache-ltfnpr {
+                color: black
+            }
+        </style>""", unsafe_allow_html=True)
+    
+    # with tab_s1:
+    #     scenario_img(scenario=1,
+    #                 augment_type={'no augmentation':'no'})
+    # with tab_s2:
+    #     scenario_img(scenario=2,
+    #                 augment_type=AUG_LIST)
+    # with tab_s3:
+    #     scenario_img(scenario=3,
+    #                 augment_type=AUG_LIST)
+
 
 ## model result
 with st.container():
     st.header("Performa Model", anchor="hasil-evaluasi")
-    st.markdown(f"<div style='text-align: justify;'>{LOREM_IPSUM100}</div><br>",
+
+    st.subheader("Berdasarkan Skenario", anchor="skenario-evaluasi")
+    tab_s1, tab_s2, tab_s3 = st.tabs(['Skenario 1',
+                                        'Skenario 2',
+                                        'Skenario 3'])
+    
+    with tab_s1:
+        show_val_result('s1')
+    with tab_s2:
+        show_val_result('s2')
+    with tab_s3:
+        show_val_result('s3')
+
+    st.subheader("Berdasarkan Dataset", anchor="dataset-evaluasi")
+    tab_ev_rimone, tab_ev_g1020, tab_ev_refuge, tab_ev_papila = st.tabs(['Rim-One',
+                                                                        'G1020',
+                                                                        'REFUGE',
+                                                                        'PAPILA'])
+    
+    with tab_ev_rimone:
+        show_val_result('rimone')
+    with tab_ev_g1020:
+        show_val_result('g1020')
+    with tab_ev_refuge:
+        show_val_result('refuge')
+    with tab_ev_papila:
+        show_val_result('papila')
+    
+    st.markdown(f"<div style='text-align: justify;'>{LOREM_IPSUM100}</div>",
                 unsafe_allow_html=True)
+    
+
 
 ## summary and conclusion
 with st.container():
     st.header("Kesimpulan", anchor="kesimpulan")
-    st.markdown(f"<div style='text-align: justify;'>{LOREM_IPSUM100}</div>",
+    st.markdown(f"<div style='text-align: justify;'>{LOREM_IPSUM100}</div><br>",
                 unsafe_allow_html=True)
 
 ## sidebar
